c314cfb804cbd741e03e096f5b47bac336a82ef7
Hadi <hadizorkot@gmail.com>
2022-05-21 20:00:59 +0000

    Added entities and combat base
    -Added Entities
    -Added Energies such as Health and Mana
    -Added Level and LevelBasedValues
    -Added some entities components (WIP)
    -Added entity types prefabs
    -Added Factions
    -Added Controller2k (external free asset) (WIP)
    -small changes to managers
    -adding test scripts to simulate combat (WIP)

---
+++ Assets/Scripts/Energies/Mana.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace GameJam
+{
+    public interface IManaBonus
+    {
+        int GetManaBonus(int baseMana);
+    }
+
+    public class Mana : Energy
+    {
+        [Header("Mana")]
+        [SerializeField] private LinearInt baseMana = new LinearInt { baseValue = 1 };
+
+        // cache components that give a bonus (attributes, inventory, etc.)
+        // (assigned when needed. NOT in Awake because then prefab.max doesn't work)
+        private IManaBonus[] _bonusComponents;
+        private IManaBonus[] bonusComponents => _bonusComponents ??= GetComponents<IManaBonus>();
+
+        public override int Max
+        {
+            get
+            {
+                int bonus = 0;
+                int baseThisLevel = baseMana.Get(level.Current);
+                foreach (IHealthBonus bonusComponent in bonusComponents)
+                {
+                    bonus += bonusComponent.GetHealthBonus(baseThisLevel);
+                }
+                return baseThisLevel + bonus;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Stats/Level.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace GameJam
+{
+    [DisallowMultipleComponent]
+    public class Level : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private int current = 1;
+        [SerializeField] private int max = 100;
+
+        public int Current => current;
+        public int Max => max;
+
+        public void SetLevel(int newLevel) => current = Mathf.Clamp(newLevel, 1, max);
+        public void SetMaxLevel(int newMaxLevel) => max = newMaxLevel;
+
+        private void OnValidate()
+        {
+            current = Mathf.Clamp(current, 1, max);
+        }
+    }
+}
